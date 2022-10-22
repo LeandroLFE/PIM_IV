012bcef999079c41e6a431c62919e3ef3af5af04
LeandroLFE <lfe.leandro@gmail.com>
2022-10-22 19:08:48 -0300

    Altera o sistemaLogin, add cor e clear screen

--- sistemaLogin/sistemaLogin.c
+++ sistemaLogin/sistemaLogin.c
@@ -5,6 +5,7 @@
 #include "../headers/headers_programa.h"
 
 //cores
+#define VERDE 2
 #define VERMELHO_CLARO 12
 #define BRANCO 15
 
@@ -19,10 +20,14 @@ int sistemaLogin(const int tamanhoMaxUsuario, const int tamanhoMaxSenha){
     hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
 
     do{
+        clearScreen(hStdout);
+        fflush(stdin);
+        setColor(VERDE);
         printf("************************************");
         printf("\n*******BEM VINDO AO SISTEMA*********");
         printf("\n**************LOGIN*****************");
         printf("\n************************************\n");
+        setColor(BRANCO);
 
         strcpy(auxUsuario, getUsuario(tamanhoMaxUsuario));
         usuario = auxUsuario;
