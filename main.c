5fb797a7ce743d40549f7ca4bc52cba85fa16cc7
LeandroLFE <lfe.leandro@gmail.com>
2022-10-24 12:42:15 -0300

    Adicionado localização para incluir acentos

--- main.c
+++ main.c
@@ -1,10 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <locale.h>
 
 #include "headers/headers_programa.h"
 
 #define TAMANHO_MAX_USUARIO 15
 #define TAMANHO_MAX_SENHA 15
+
+#define TAMANHO_MAX_NOME 52
+#define TAMANHO_MAX_RUA 152
+#define TAMANHO_MAX_NUMERO 6
+#define TAMANHO_MAX_BAIRRO_CIDADE 102
+#define TAMANHO_MAX_COMPLEMENTO 72
+
 
 //cores
 #define CIANO 3
@@ -15,17 +23,16 @@ int main(void){
     int login;
     int formulario = 1;
 
+    setlocale(LC_ALL, "portuguese");
     setColor(BRANCO);
+
     do{
         login = sistemaLogin(TAMANHO_MAX_USUARIO, TAMANHO_MAX_SENHA);
 
-        if(login != 0){
-            printfColorido("\n\nUsuario/Senha invalidos, tente novamente\n\n", VERMELHO_CLARO);
-        }
-
         printfColorido("\n\nLogin realizado com sucesso!\n\n", CIANO);
 
-        formulario = sistemaFormulario(50, 50, 50, 50, 50);
+        formulario = sistemaFormulario(TAMANHO_MAX_NOME, TAMANHO_MAX_RUA, TAMANHO_MAX_NUMERO,
+                                       TAMANHO_MAX_COMPLEMENTO, TAMANHO_MAX_BAIRRO_CIDADE, 50, 50);
 
     }while(formulario==1);
 
